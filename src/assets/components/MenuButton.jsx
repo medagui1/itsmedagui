5cd5d84dcc8d9855cc9ab9b54bae6552a9013ec6
Med Agui <simohamedregragui2@gmail.com>
2024-01-11 00:08:57 +0100

    adding animated menu button

---
+++ src/assets/components/MenuButton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import '../../App.css'
+
+const MenuButton = () => {
+  return (
+    <button className="button" id="menu-button">
+      <span></span>
+      <span></span>
+      <span></span>
+    </button>
+  );
+};
+
+export default MenuButton;
--- src/assets/sections/Nav.jsx
+++ src/assets/sections/Nav.jsx
@@ -1,29 +1,40 @@
-import Button from "../components/Button"
-import { navLinks } from "../constants/links"
+import Button from "../components/Button";
+import { navLinks } from "../constants/links";
+import '../../App.css'
+import MenuButton from "../components/MenuButton";
 
 const Nav = () => {
   return (
-    <nav className="flex justify-between items-center  py-4 px-16 max-sm:px-12 max-sm:py-8" data-aos='fade-down'>
+    <div
+      className="flex justify-between items-center  py-8 px-16 max-sm:px-8 max-sm:py-8"
+      data-aos="fade-down"
+    >
       <div className="flex gap-16 items-center">
-        <h1 className="font-bold text-2xl">MedAgui.</h1>
+        <a href="/">
+          <h1 className="font-bold text-2xl  logo duration-300 cursor-pointer">
+            MedAgui.
+          </h1>
+        </a>
         <div className="flex gap-8 max-lg:hidden text-lg">
-          {navLinks.map(link => (
-            <a key={link.name}  href={link.link} className="hover:text-[#b5b5b5] transition-colors duration-300">{link.name}</a>
+          {navLinks.map((link) => (
+            <a
+              key={link.name}
+              href={link.link}
+              className="hover:text-[#b5b5b5] transition-colors duration-300"
+            >
+              {link.name}
+            </a>
           ))}
         </div>
       </div>
       <div className="max-sm:hidden">
-        <Button label={"LET'S TALK"} isInversed={true}/>
+        <Button label={"LET'S TALK"} isInversed={true} />
       </div>
       <div className="sm:hidden">
-        <button>
-          <img src="icons/hamburger.svg" alt="menu" className="w-6 h-6 dark:fill-white"/>
-        </button>
+        <MenuButton />
       </div>
-      
-    </nav>
-    
-  )
-}
+    </div>
+  );
+};
 
-export default Nav
+export default Nav;
