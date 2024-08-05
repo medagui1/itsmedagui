22c975e722b80234e79d18114fa06cd5c8ab28d3
Med Agui <simohamedregragui2@gmail.com>
2024-08-05 14:51:05 +0100

    adding the upgrade soon banner

---
+++ src/assets/components/UpgradeSoonBanner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UpgradeSoonBanner = () => {
+  return (
+    <div className='bg-red-500 w-full h-16 flex justify-center items-center fixed top-0 left-0 z-[99999] '>
+        <p className='font-[500] '>Upgrade Coming Soon</p>
+    </div>
+  )
+}
+
+export default UpgradeSoonBanner
