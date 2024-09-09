681b60a422ae08e2ae2c0d1bc437839a559a1d1b
Med Agui <simohamedregragui2@gmail.com>
2024-09-09 23:38:32 +0100

    changing the project card layour

--- src/assets/components/ProjectCard.jsx
+++ src/assets/components/ProjectCard.jsx
@@ -11,9 +11,11 @@ import { splitTextIntoSpans } from "../utilities/splitTextIntoSpans";
 const ProjectCard = ({ project, index }) => {
   const [transformOrigin, setTransformOrigin] = useState("50% 50%");
   const imgContainerRef = useRef(null);
-  const imgRef = useRef(null)
-  const projectNameRef = useRef(null)
+  const imgRef = useRef(null);
+  const projectNameRef = useRef(null);
 
+  // this function is responsible for calculating the transform origin percents
+  // so that there is a smooth on hover effect on images
   const handleMouseMove = (e) => {
     const rect = imgContainerRef.current.getBoundingClientRect();
     const x = e.clientX - rect.left; // Get the x coordinate relative to the image
@@ -27,7 +29,9 @@ const ProjectCard = ({ project, index }) => {
     setTransformOrigin(`${originX}% ${originY}%`);
   };
 
-  useEffect(() => {
+  useGSAP(() => {
+    splitTextIntoSpans(projectNameRef, "");
+
     const imageSelector = document.querySelector(`.project-img-${index}`);
 
     gsap.fromTo(
@@ -38,9 +42,9 @@ const ProjectCard = ({ project, index }) => {
         transformOrigin: "left bottom",
       },
       {
+        duration: 0.4,
         scale: 1,
         opacity: 1,
-        duration: 0.4,
         scrollTrigger: {
           trigger: imageSelector,
           start: "top bottom",
@@ -48,46 +52,66 @@ const ProjectCard = ({ project, index }) => {
         },
       }
     );
-    return () => {
-      ScrollTrigger.getById(imageSelector)?.kill();
-    };
-  }, [index]);
 
-  useGSAP(() => {
-    splitTextIntoSpans(projectNameRef, '')
-    const tl = gsap.timeline({scrollTrigger : {
-      trigger: `.project-name-${index}`,
-    start: 'top 90%',
-    }})
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: `.project-name-${index}`,
+        start: "top 95%",
+        end: "top 80%",
+        scrub: 1,
+      },
+    });
+
     tl.fromTo(
       `.project-name-${index} span`,
       {
         opacity: 0,
-        scale : 0.2
+        scale: 0.2,
       },
       {
         opacity: 1,
-        scale : 1,
+        scale: 1,
         duration: 0.2,
         stagger: 0.1,
       }
     );
-    tl.fromTo(`.arrow-container-${index}`, {
-      opacity: 0,
-      scale : 0.2
-    },
-    {
-      opacity: 1,
-      scale : 1,
-      duration: 0.2,
-    })
-    ScrollTrigger.refresh()
-  }, [])
+    tl.fromTo(
+      `.arrow-container-${index}`,
+      {
+        opacity: 0,
+        scale: 0.2,
+      },
+      {
+        opacity: 1,
+        scale: 1,
+        duration: 0.2,
+      }
+    );
+
+    ScrollTrigger.refresh();
+  }, []);
 
   return (
-    <div className="sticky top-[72px] left-0 bg-black_primary">
+    <div
+      className={`sticky bg-black_primary`}
+      style={{ top: `${index * 100 + 72}px` }}
+    >
       <hr className="bg-white_primary h-[1px] mt-8" />
-      <div className="flex py-4 flex-wrap sticky -top-[20vh] ">
+      <div className="flex py-4 flex-wrap">
+        <div className="w-full py-4 pr-4 flex gap-4 items-center">
+          <a className="flex gap-4 items-first-baseline my-2" href="#">
+            <h3
+              className={`title text-right text-3xl project-name-${index}`}
+              ref={projectNameRef}
+            >
+              {" "}
+              {project.name}{" "}
+            </h3>
+            <div className={`arrow-container-${index}`}>
+              <ProjectCardArrow />
+            </div>
+          </a>
+        </div>
         <div
           className={`overflow-hidden project-img-${index}`}
           ref={imgContainerRef}
@@ -101,55 +125,9 @@ const ProjectCard = ({ project, index }) => {
             ref={imgRef}
           />
         </div>
-        <div className="w-full py-4 pr-4 flex gap-4 items-center">
-          {/* <div className="w-full h-[1px] bg-white_primary"></div> */}
-          <a className="flex gap-4 items-first-baseline my-2" href="#">
-            <h3 className={`title text-right text-3xl project-name-${index}`} ref={projectNameRef}> {project.name} </h3>
-            <div className={`arrow-container-${index}`}>
-            <ProjectCardArrow />
-            </div>
-          </a>
-        </div>
-        {/* <div className="flex flex-wrap gap-2">
-          {project.tags.map((tag, index) => (
-            <TagCard tag={tag} />
-          ))}
-        </div> */}
       </div>
     </div>
   );
-
-  // return (
-  //   <div className="flex p-6 shadow-xl bg-white_secondary dark:bg-black_secondary rounded-xl max-lg:flex-col gap-12 " data-aos='fade-down'>
-  //     <div className="group max-h-[800px] w-[50%]  max-lg:w-full  overflow-hidden  rounded-[12px] ">
-  //     <img
-  //       loading="lazy"
-  //       src={project.image}
-  //       alt=""
-  //       className="w-full object-cover scale-[1] transition-scale group-hover:scale-[1.2] duration-300 overflow-hidden "
-  //       width={550}
-  //       height={830}
-  //     />
-  //     </div>
-  //     <div className="flex-1 flex flex-col gap-4">
-  //       <h3 className="font-bold text-2xl">{project.name}</h3>
-  //       <p>{project.description}</p>
-  //       <div className="flex flex-wrap gap-2">
-  //         {project.tags?.map((tag) => (
-  //           <TagCard tag={tag} />
-  //         ))}
-  //       </div>
-  //       <div className="flex gap-8 mt-8 flex-wrap max-sm:gap-4">
-  //         <button aria-label="View Live" className="  bg-black_primary dark:bg-white_primary px-8 py-4 text-white_secondary dark:text-black_secondary rounded-xl font-semibold transition-[transform] hover:scale-[1.1] duration-300">
-  //           <a href={project.live}>Website</a>
-  //         </button>
-  //         <button aria-label="Code in Github" className="bg-white_secondary dark:bg-black_secondary hover:scale-[1.1] px-8 py-4 text-black_secondary dark:text-white_secondary rounded-xl font-semibold border-[1px] border-black_secondary dark:border-white_secondary transition-[transform] scale-100 duration-300">
-  //           <a href={project.code}>Github</a>
-  //         </button>
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
 };
 
 export default ProjectCard;
