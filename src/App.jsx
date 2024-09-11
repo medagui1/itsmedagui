a1883d68a84f84f0e8ed668c04049a99005c2f20
Med Agui <simohamedregragui2@gmail.com>
2024-09-11 17:52:45 +0100

    passing the headerHeight to the Header and Hero from App

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,30 @@
-import { useEffect, useState } from "react";
 import "./App.css";
+import CardStacking from "./assets/sections/CardStacking";
 import {
-  AboutMe,
-  CallToAction,
-  Footer,
+  Header,
+  CustomCursor,
   Hero,
-  Nav,
   Works,
 } from "./assets/sections/index";
-import Header from "./assets/sections/Header";
-import Portfolio from "./assets/sections/Works";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
-import CustomCursor from "./assets/components/CustomCursor";
 
+// registering ScrollTrigger plugin
 gsap.registerPlugin(ScrollTrigger)
 
 function App() {
 
-  const [isScrolled, setIsScrolled] = useState(false)
-
-  // gsap.globalTimeline;
-
+  const headerHeight = 104
   return (
-    <main className="">
-      <Header />
-      <Hero />
-      <Portfolio />
+    <main>
+      <Header headerHeight={headerHeight}/>
+      <Hero headerHeight={headerHeight}/>
+      <Works />
       <CustomCursor />
     </main>
-    // <main className="font-raleway dark:text-white text-black_primary bg-white_primary dark:bg-black_primary max-w-[1280px] " style={{overflowX : 'hidden'}}>
-    //   <UpgradeSoonBanner />
-    //   <nav className={`z-50 mt-16 ${isScrolled && "nav-shadow"}`}>
-    //     <Nav />
-    //   </nav>
-    //   <Hero />
-    //   <Works />
-    //   <AboutMe />
-    //   <section>
-    //     <CallToAction />
-    //   </section>
-    //     <Footer />
-    // </main>
   );
 }
 
 export default App;
+
+// https://codepen.io/snorkltv/pen/GRwqWeo
